99af80c5eb356481afa1ce1011798a0952aabb35
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-06-10 08:50:35 +0900

    Update 03.StackView.swift
    주석 추가

--- tutorial/03.StackView.swift
+++ tutorial/03.StackView.swift
@@ -11,8 +11,9 @@ import SwiftUI
 
 struct StackView: View {
     var body: some View {
-        // alignment: .leading: 좌측 정렬, .trailing: 우측 정렬
-        // .spacing: 하위 view 간 간격
+        // parameter 설명:
+        // 1. alignment: .leading: 좌측 정렬, .trailing: 우측 정렬
+        // 2. spacing: 하위 view 간 간격을 정하는 파라미터.
         VStack(alignment: .trailing, spacing: 30) {
             Text("Hello")
                 .background(Color.yellow)
@@ -31,12 +32,14 @@ struct StackView: View {
         .font(.system(size: 50))
         .foregroundColor(.red)
         
-        // 이미지가 가장 하위에 위치해야 하므로 ZStack{} 내부에 Image를 넣음
+        // 이미지가 가장 하위에 위치해야 하므로 ZStack{} 내부 가장 첫 번째에 Image를 넣음.
+        // ZStack의 경우 스택 내부에 삽입된 순서대로 아래에서 위 방향으로 객체가 쌓인다.
         ZStack {
             Image("500500")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
             
+            // 에디트를 위한 버튼 EditButton() 배치. EditButton()은 하단에서 따로 작성한 후 여기 블러와서 사용한다.
             // EditButton()이 Image보다 더 위의 레이어에 위치해야 버튼을 누를 수 있으므로 Image 코드보다 아래쪽에 입력함.
             EditButton()
         }
