65836f49474d0bc976228ff9da9f7fbea4e8482b
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-08-10 14:56:41 +0900

    ### Motivation - 그간 방치했던 SwiftUI 튜토리얼 개선
    ### Key Changes
    - 애플 공식 튜토리얼의 정확한 번역으로 튜토리얼을 변경
    - 각 코드의 번호를 따라가며 학습할 수 있도록 리팩터링
    
    ### To Reviewers
    - 없음

---
+++ tutorial/MainContentView.swift
@@ -0,0 +1,23 @@
+//
+//  MainContentView.swift
+//  tutorial
+//
+//  Created by 김제필 on 8/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
--- tutorial/tutorialApp.swift
+++ tutorial/tutorialApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct tutorialApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // 원래 이 아래에는 ContentView()가 지정돼 있었을 것이다.
+            // 하지만 나는 이 파일을 수정하지 않고, 다른 파일에 연결하기 위해 MainContentView()를 불러오도록 변경하였다.
+            // MainContentView 파일로 가보자.
+            MainContentView()
         }
     }
 }
