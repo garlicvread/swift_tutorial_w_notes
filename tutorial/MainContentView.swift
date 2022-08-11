2cb5dead11416ea3dfd050b7434b3d777c81470e
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-08-11 10:30:08 +0900

    Refactor: Section 3 Combine Views Using Stacks
    ### Motivation
    - Section 3 Combine Views Using Stacks 예제코드 및 주석 추가
    
    ### Key Changes
    - Section 3 Combine Views Using Stacks 예제코드 및 주석 추가
    
    ### To Reviewers
    - 없음

--- tutorial/MainContentView.swift
+++ tutorial/MainContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 
 struct MainContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.black)
-            .padding()
+
+        // 3.1., 3.2. VStack 추가
+        VStack(alignment: .leading) {  // 3.5. alignment 옵션 추가
+            Text("Turtle Rock")
+                .font(.title)
+
+            // 3.6. HSTack 추가
+            HStack {
+                Text("Joshua Tree National Park")  // 3.3. TextView의 placeholder 변경
+                    .font(.subheadline)  // 3.4. 폰트 변경
+
+                Spacer()  // 3.8. Spacer 추가
+
+                Text("California")  // 3.7. TextView 추가
+                    .font(.subheadline)
+            }
+        }
+        .padding()  // 3.9. 패딩 추가
     }
 }
 
