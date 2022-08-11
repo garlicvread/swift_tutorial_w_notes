a53bd245f101d377662670ec370d87cf677deb52
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-08-11 13:22:52 +0900

    Refactor: Section 2.1 update
    ### Motivation
    - Section 2.1. Create a Landmark Model
    
    ### Key Changes
    - Section 2.1. Create a Landmark Model 예제 코드 및 주석 추가
    
    ### To Reviewers
    - 없음

---
+++ tutorial/Model/ModelData.swift
@@ -0,0 +1,40 @@
+//
+//  ModelData.swift
+//  tutorial
+//
+//  Created by 김제필 on 8/11/22.
+//
+
+import Foundation
+
+// 2.1.10 landmarkData.json의 데이터를 받아올 어레이 선언
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// 2.1.9. 애플리케이션의 메인 번들에서 지정된 이름의 json 데이터를 가져오는 메서드 생성
+// load 메서드는 Codable 프로토콜의 구성요소인 Decodable 프로토콜의 반환 타입을 따른다.
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename,
+                                     withExtension: nil)
+    else {
+        fatalError("Canot find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Cannot load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Cannot parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
+// 여기까지 작업이 끝났다면 디렉토리를 정리하자.
+// 어떤 디렉토리에 어떤 파일을 배치할 것인지는 공식 문서를 보면 나옴.
--- tutorial/Views/MapView.swift
+++ tutorial/Views/MapView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
-import MapKit  // 5.2. MapKit 프레임워크 가져오기
+import MapKit  // 1.5.2. MapKit 프레임워크 가져오기
 
 struct MapView: View {
 
-    // 5.3. 위치 정보를 담는 변수 region에 @State 옵션 추가
+    // 1.5.3. 위치 정보를 담는 변수 region에 @State 옵션 추가
     // @State 옵션(attribute)를 추가하는 경우 그 변수의 데이터가 변동되면 자동으로 뷰가 업데이트된다.
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286,
@@ -20,13 +20,13 @@ struct MapView: View {
     )
 
     var body: some View {
-        Map(coordinateRegion: $region)  // 5.4. MapView 추가
+        Map(coordinateRegion: $region)  // 1.5.4. MapView 추가
         // @State private var region 값을 받아옴.
     }
 }
 
 
-// 5.5. Previews 가 static 선언돼 있는 경우 native SwiftUI view만을 렌더링한다.
+// 1.5.5. Previews 가 static 선언돼 있는 경우 native SwiftUI view만을 렌더링한다.
 // MapView를 보려면 라이브 프리뷰 버튼을 눌러야 함.
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
